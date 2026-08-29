e0035cd1b9369f18ba365f652e5cb98a8f2334b9
starmis2-lgtm <star.mis2@starglobal.in>
2026-08-29 17:07:42 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// ===== GITHUB REPO  ·  file name: sw.js =====
+const CACHE = 'checklist-v5';
+const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) {
+    return c.addAll(SHELL);
+  }).then(function () {
+    return self.skipWaiting();
+  }));
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) {
+      return k === CACHE ? null : caches.delete(k);
+    }));
+  }).then(function () {
+    return self.clients.claim();
+  }));
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.indexOf('script.google.com') !== -1) return;
+  e.respondWith(
+    fetch(e.request).then(function (res) {
+      const copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () {
+      return caches.match(e.request).then(function (hit) {
+        return hit || caches.match('./index.html');
+      });
+    })
+  );
+});
