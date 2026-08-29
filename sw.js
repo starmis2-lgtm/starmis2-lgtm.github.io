92271eee0dcae344e92cf9c7c30dd57935229303
starmis2-lgtm <star.mis2@starglobal.in>
2026-08-29 17:29:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== GITHUB REPO  ·  file name: sw.js =====
-const CACHE = 'checklist-v7';
+const CACHE = 'checklist-v8';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (e) {
